e5c953399863fbd763be81ce76e60730f494f95b
Maks Kravchenko <krava.zf@gmail.com>
2023-06-07 17:10:20 +0300

    heroes-render-done

---
+++ markup/index.js
@@ -0,0 +1,46 @@
+let marvelHeroes = [
+	{
+		name: "Thor"
+	},
+	{
+		name: "Spider Man"
+	}
+];
+
+let dcHeroes = [
+	{
+		name: "Superman"
+	},
+	{
+		name: "Batman"
+	},
+	{
+		name: "Deadpool"
+	}
+];
+
+Array.prototype.heroesRender = function(folderName) {
+	let tableHTML = '<table>';
+	tableHTML += '<thead><tr><th>Name</th><th>Icon</th></tr></thead>';
+	tableHTML += '<tbody>';
+
+	this.forEach(function(hero) {
+		tableHTML += '<tr>';
+		tableHTML += '<td>' + hero.name + '</td>';
+		tableHTML += '<td>';
+		let imageName = hero.name.replace(/\s/g, '').toLowerCase() + '.svg';
+		let imagePath = 'images/' + folderName + '/' + imageName;
+		tableHTML += '<img src="' + imagePath + '">';
+		tableHTML += '</td>';
+		tableHTML += '</tr>';
+	});
+
+	tableHTML += '</tbody>';
+	tableHTML += '</table>';
+
+	document.write(tableHTML);
+};
+
+marvelHeroes.heroesRender('marvel');
+dcHeroes.heroesRender('dc');
+
