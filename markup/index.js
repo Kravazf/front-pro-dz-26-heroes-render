c8fe21bff641a65ee2d1ef17a5c64b50819680ea
Maks Kravchenko <krava.zf@gmail.com>
2023-06-07 18:52:52 +0300

    Heroes-render-done-2-variant

--- markup/index.js
+++ markup/index.js
@@ -1,4 +1,52 @@
-let marvelHeroes = [
+// let marvelHeroes = [
+// 	{
+// 		name: "Thor"
+// 	},
+// 	{
+// 		name: "Spider Man"
+// 	}
+// ];
+
+// let dcHeroes = [
+// 	{
+// 		name: "Superman"
+// 	},
+// 	{
+// 		name: "Batman"
+// 	},
+// 	{
+// 		name: "Deadpool"
+// 	}
+// ];
+
+// Array.prototype.heroesRender = function(folderName) {
+// 	let tableHTML = '<table>';
+// 	tableHTML += '<thead><tr><th>Name</th><th>Icon</th></tr></thead>';
+// 	tableHTML += '<tbody>';
+
+// 	this.forEach(function(hero) {
+// 		tableHTML += '<tr>';
+// 		tableHTML += '<td>' + hero.name + '</td>';
+// 		tableHTML += '<td>';
+// 		let imageName = hero.name.replace(/\s/g, '').toLowerCase() + '.svg';
+// 		let imagePath = 'images/' + folderName + '/' + imageName;
+// 		tableHTML += `<img src= ${imagePath}>`;
+// 		tableHTML += '</td>';
+// 		tableHTML += '</tr>';
+// 	});
+
+// 	tableHTML += '</tbody>';
+// 	tableHTML += '</table>';
+
+// 	document.write(tableHTML);
+// };
+
+// marvelHeroes.heroesRender('marvel');
+// dcHeroes.heroesRender('dc');
+
+
+
+const marvelHeroes = [
 	{
 		name: "Thor"
 	},
@@ -7,7 +55,7 @@ let marvelHeroes = [
 	}
 ];
 
-let dcHeroes = [
+const dcHeroes = [
 	{
 		name: "Superman"
 	},
@@ -20,27 +68,31 @@ let dcHeroes = [
 ];
 
 Array.prototype.heroesRender = function(folderName) {
-	let tableHTML = '<table>';
-	tableHTML += '<thead><tr><th>Name</th><th>Icon</th></tr></thead>';
-	tableHTML += '<tbody>';
-
-	this.forEach(function(hero) {
-		tableHTML += '<tr>';
-		tableHTML += '<td>' + hero.name + '</td>';
-		tableHTML += '<td>';
-		let imageName = hero.name.replace(/\s/g, '').toLowerCase() + '.svg';
-		let imagePath = 'images/' + folderName + '/' + imageName;
-		tableHTML += '<img src="' + imagePath + '">';
-		tableHTML += '</td>';
-		tableHTML += '</tr>';
+	const tableRows = this.map(function(hero) {
+		const imageName = hero.name.replace(/\s/g, '').toLowerCase() + '.svg';
+		const imagePath = 'images/' + folderName + '/' + imageName;
+		return '<tr>\
+			<td>' + hero.name + '</td>\
+			<td>\
+				<img src="' + imagePath + '">\
+			</td>\
+		</tr>';
 	});
 
-	tableHTML += '</tbody>';
-	tableHTML += '</table>';
+	const tableHTML = '<table>\
+		<thead>\
+			<tr>\
+				<th>Name</th>\
+				<th>Icon</th>\
+			</tr>\
+		</thead>\
+		<tbody>\
+			' + tableRows.join('') + '\
+		</tbody>\
+	</table>';
 
 	document.write(tableHTML);
 };
 
 marvelHeroes.heroesRender('marvel');
 dcHeroes.heroesRender('dc');
-
